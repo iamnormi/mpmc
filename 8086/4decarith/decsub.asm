4f9f37d922ea18646285777c0e3aa9199eb24395
1437k <71950371+iamvk1437k@users.noreply.github.com>
2022-01-02 21:34:16 +0530

    Update decsub.asm

--- 8086/4decarith/decsub.asm
+++ 8086/4decarith/decsub.asm
@@ -1,5 +1,5 @@
-MOV AL,#15  ; load the value of AL
-MOV BL,#08  ; load the value of BL
+MOV AL,15  ; load the value of AL
+MOV BL,08  ; load the value of BL
 SUB AL,BL   ; AL=AL-BL
 DAA         ; adjust the decimal after subtraction
 RET
