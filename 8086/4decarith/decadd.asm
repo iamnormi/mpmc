c5e25d9e5d73d515393b073c474b8abf7fd5f4b4
1437k <71950371+iamvk1437k@users.noreply.github.com>
2022-01-02 21:33:57 +0530

    Update decadd.asm

--- 8086/4decarith/decadd.asm
+++ 8086/4decarith/decadd.asm
@@ -1,5 +1,5 @@
-MOV AL,#15  ; load the value of AL
-MOV BL,#08  ; load the value of BL
+MOV AL,15  ; load the value of AL
+MOV BL,08  ; load the value of BL
 ADD AL,BL   ; AL=AL+BL
 DAA         ; adjust the decimal after addition
 RET
